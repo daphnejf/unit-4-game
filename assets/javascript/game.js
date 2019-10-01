dd5a5d852115c5f48a0d83dfc26eead934ce50f0
Daphne Jiménez <daphnejimenezf@gmail.com>
2019-10-01 16:12:05 -0500

    fixed reset

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -13,6 +13,23 @@ var cr4 = Math.floor(Math.random()*12) + 1;
 var c = [cr1,cr2,cr3,cr4];
 console.log(c);
 
+function reset() {
+    totalScore = 0;
+    $("#totalScore").text(totalScore);  
+
+    randomNumber = Math.floor(Math.random() * (120 - 20 + 1)) + 20;
+    $("#randomNumber").text(randomNumber);
+    console.log(randomNumber);
+
+    cr1 = Math.floor(Math.random()*12) + 1;
+    cr2 = Math.floor(Math.random()*12) + 1;
+    cr3 = Math.floor(Math.random()*12) + 1;
+    cr4 = Math.floor(Math.random()*12) + 1;
+    
+    var c = [cr1,cr2,cr3,cr4];
+    console.log(c);
+}
+
 function win() {
     wins++;
     $("#wins").text("Wins: " + wins);
@@ -69,15 +86,3 @@ $("#crystal4").click(() => {
     }
 });
 
-function reset() {
-    totalScore = 0;
-    $("#totalScore").text(totalScore);   
-    var randomNumber = Math.floor(Math.random() * (120 - 20 + 1)) + 20;
-    $("#randomNumber").text(randomNumber);
-    var cr1 = Math.floor(Math.random()*12) + 1;
-    var cr2 = Math.floor(Math.random()*12) + 1;
-    var cr3 = Math.floor(Math.random()*12) + 1;
-    var cr4 = Math.floor(Math.random()*12) + 1;
-    var c = [cr1,cr2,cr3,cr4];
-    console.log(c);
-}
